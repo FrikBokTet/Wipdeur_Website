8c638436b70ec1c2975adc2a9f04b5219cc611c2
Karel <karelliebenberh21@gmail.com>
2024-04-09 15:54:03 +0200

    Removed the old images layout and turned it into a slideshow

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
         icon.classList.toggle('fa-plus');
         icon.classList.toggle('fa-minus');
 
-        // Toggle Visibilty Of Body
+        // Toggle Visibility Of Body
         groupBody.classList.toggle('open');
 
         // Close Other Open FAQ Bodies
@@ -42,3 +42,33 @@ document.addEventListener('DOMContentLoaded', () => {
 
     hamburgerButton.addEventListener('click', () => mobileMenu.classList.toggle('active'));
 })
+
+//Doors Slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("doorSlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
